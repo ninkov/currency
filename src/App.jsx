97e4c2031dae53ef52322198602c610d43eeb4b1
Ninkov <ninkov82@gmail.com>
2024-09-13 11:16:07 +0300

    part of the app's main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import "./App.css";
+import InputBox from "./components/index";
 
 function App() {
-
-
   return (
-    <>
-        <h1 className='bg-red-300'>test</h1>
-    </>
-  )
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url(https://images.pexels.com/photos/730547/pexels-photo-730547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+      }}
+    >
+      <InputBox />
+    </div>
+  );
 }
 
-export default App
+export default App;
